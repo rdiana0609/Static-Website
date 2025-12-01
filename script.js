577096e5a81fe6fe410ec109fd3a0fbcfa9db59a
rdiana0609 <diana.maria.rednic@stud.ubbcluj.ro>
2025-12-01 15:29:55 +0200

    fixed space between slides per view

--- script.js
+++ script.js
@@ -23,8 +23,9 @@ const swiper = new Swiper('.mySwiper', {
     clickable: true,
   },
   breakpoints: {
-    0: { slidesPerView: 1 , spaceBetween: 5},
-    768: { slidesPerView: 2 },
+    0: { slidesPerView: 1 , spaceBetween: 6},
+    400:{ slidesPerView: 2, spaceBetween: 10 },
+    768: { slidesPerView: 2, spaceBetween: 10 },
     992: { slidesPerView: 3 }
   }
 });
